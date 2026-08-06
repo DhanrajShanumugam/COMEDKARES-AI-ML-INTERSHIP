9cbfb97db1c66ea09794b5aa6612d3f50c0fc686
DhanrajShanumugam <dhanrajsg27@gmail.com>
2026-08-06 13:17:52 +0530

    Update designchallenge.py

--- Day2/designchallenge.py
+++ Day2/designchallenge.py
@@ -1,1 +1,3 @@
-
+subjects=("python","sql","ai")
+student=[] def add_student(name,age): student_details={ "name":"dhanraj", "age":20, "subject":subjects } student.append(student_details) add_student("name","age")
+#add_student("Dhanraj",20) #add_student("vinooj",21) for student_details in student: print(student_details)
